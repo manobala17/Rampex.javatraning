c701c9455af370a44c2b9847efafa1cf027664b5
manobala <manobala2917@gmail.com>
2026-07-06 10:15:49 +0530

    program was created, using the print function to print the statement

---
+++ src/Day_1_Class_Task/Welcome.java
@@ -0,0 +1,9 @@
+package Day_1_Class_Task;
+
+public class Welcome {
+        public static void main(String[] args)       // this is the function
+        {
+            System.out.println("WELCOME");
+        }
+    }
+
