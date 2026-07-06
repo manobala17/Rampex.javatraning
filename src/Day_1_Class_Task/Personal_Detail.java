b1b6f27310bf984cf0f831748d74fcc7838f4e6d
manobala <manobala2917@gmail.com>
2026-07-06 10:19:55 +0530

    program was created, using the print function to print my details

---
+++ src/Day_1_Class_Task/Personal_Detail.java
@@ -0,0 +1,12 @@
+package Day_1_Class_Task;
+
+public class Personal_Detail {
+        public static void main(String[] args)
+        {
+            char initial = 'N';
+            String name = "Manobala";
+            int age =19;
+            double cgpa = 8.4;
+            System.out.println(" My name is " + name+"."+initial + " of age "+ age + " with the cgpa of " + cgpa);
+        }
+}
