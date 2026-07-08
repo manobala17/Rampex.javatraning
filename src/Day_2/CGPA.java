57640a7b3b3beb9743ab2f874595609b7727d127
manobala <manobala2917@gmail.com>
2026-07-08 21:08:07 +0530

    program was created,for to print the CGPA

---
+++ src/Day_2/CGPA.java
@@ -0,0 +1,11 @@
+package Day_2;
+
+public class CGPA {
+        public static void main(String[] args)
+        {
+            int age =19;
+            double cgpa = 8.4;
+            System.out.println("My age is "+ age + " and cgpa is " + cgpa);
+        }
+    }
+    
