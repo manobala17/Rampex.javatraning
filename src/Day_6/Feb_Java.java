0139fa72daa671b5af8fdbe48625d9cea3ac723e
manobala <manobala2917@gmail.com>
2026-07-11 21:05:57 +0530

    the program was created

---
+++ src/Day_6/Feb_Java.java
@@ -0,0 +1,25 @@
+package Day_6;
+
+public class Feb_Java {
+
+        public static void main(String[] args) {
+
+            for (int month = 1; month <= 12; month++) {
+
+                if (month == 2) {
+                    continue; // Skip February
+                }
+
+                for (int week = 1; week <= 4; week++) {
+
+                    for (int day = 1; day <= 7; day++) {
+
+                        System.out.println("Month " + month +
+                                " Week " + week +
+                                " Day " + day);
+                    }
+                }
+            }
+        }
+    }
+
