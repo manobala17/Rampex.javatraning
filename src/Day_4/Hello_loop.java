db4be0f401dc7b6625320712498c5809421a64ba
manobala <manobala2917@gmail.com>
2026-07-11 21:04:19 +0530

    the program was created

---
+++ src/Day_4/Hello_loop.java
@@ -0,0 +1,10 @@
+package Day_4;
+
+public class Hello_loop {
+        public static void main(String[] args){
+            for(int i=1;i<20;i++){
+                System.out.println("hello");
+            }
+        }
+    }
+
