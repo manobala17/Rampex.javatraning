f2958d0b6f7ac9cb7cb41b64bfd4ff76be8ab4ac
manobala <manobala2917@gmail.com>
2026-07-11 13:58:46 +0530

    the sumofoddnumber program was created

---
+++ src/Day_5/SumOfOddNumber.java
@@ -0,0 +1,21 @@
+package Day_5;
+import java.util.Scanner;
+public class SumOfOddNumber {
+
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter the number: ");
+            int n = sc.nextInt();
+            int sum = 0;
+
+            for(int i = 1; i <= n; i++) {
+                if(i % 2 != 0) {
+                    sum = sum + i;
+                }
+            }
+
+            System.out.println(sum);
+        }
+    }
+
