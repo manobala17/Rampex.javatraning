73df0a9bf537be4c4d86b41d6774228512d94521
manobala <manobala2917@gmail.com>
2026-07-11 14:01:30 +0530

    the Labelled break program was created

---
+++ src/Day_7_Class_Task/Labelled_Break.java
@@ -0,0 +1,20 @@
+package Day_7_Class_Task;
+
+public class Labelled_Break {
+
+    public static void  main (String[] args){
+
+        for(int week =1;week<=4;week++){
+
+            for(int day =1;day<=7;day++){
+
+                if(day==3){
+                    break;
+                }
+
+                System.out.println("week" + week +"day" + day);
+            }
+        }
+    }
+
+}
