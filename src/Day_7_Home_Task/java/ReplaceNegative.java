811891ae97006a676b8e2d825c7a214aa745844f
manobala <manobala2917@gmail.com>
2026-07-11 14:03:17 +0530

    the replacement program was created

---
+++ src/Day_7_Home_Task/java/ReplaceNegative.java
@@ -0,0 +1,35 @@
+package Day_7_Home_Task.java;
+import java.util.Scanner;
+public class ReplaceNegative {
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Enter array size: ");
+            int n = sc.nextInt();
+
+            int[] arr = new int[n];
+
+            System.out.println("Enter array elements:");
+
+            // Input array
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            // Replace negative numbers with 0
+            for (int i = 0; i < n; i++) {
+                if (arr[i] < 0) {
+                    arr[i] = 0;
+                }
+            }
+
+            // Print modified array
+            System.out.println("Modified array:");
+
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+
