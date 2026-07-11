9ebc7e61cf009e3aaf054634e981206b39066cf5
manobala <manobala2917@gmail.com>
2026-07-11 14:02:14 +0530

    the Array String program was created

---
+++ src/Day_7_Class_Task/Array_Task/Array_String.java
@@ -0,0 +1,17 @@
+package Day_7_Class_Task.Array_Task;
+import java.util.Arrays;
+public class Array_String {
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[5];
+
+        for (int i = 0; i <= 4; i++) {
+            arr[i] = i+2;
+
+        }
+        System.out.println(arr);
+    }
+
+
+}
