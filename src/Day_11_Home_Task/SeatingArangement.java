a2a89b74775cdc8c0558b9411454290a4380c3c6
manobala <manobala2917@gmail.com>
2026-07-14 14:47:29 +0530

    Seating Arangement program was created

---
+++ src/Day_11_Home_Task/SeatingArangement.java
@@ -0,0 +1,13 @@
+package Day_11_Home_Task;
+
+public class SeatingArangement {
+    public static void main(String[] args){
+
+        for(int row = 1;row<=5;row++){
+            for(int seat = 1;seat<=6;seat++){
+                System.out.println("R"+ row + "S" + seat + " ");
+            }
+            System.out.println();
+        }
+    }
+}
