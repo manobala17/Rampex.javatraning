3c20a0887f309d5228e143ff47344406b5c9e0a6
manobala <manobala2917@gmail.com>
2026-07-14 14:44:01 +0530

    multiThreading program was created

---
+++ src/Day_14_Class_Task/MultiThreading.java
@@ -0,0 +1,31 @@
+package Day_14_Class_Task;
+
+public class MultiThreading {
+
+
+        public static void main(String[] args) {
+
+            MyThread mt = new MyThread();
+
+            Thread t1 = new Thread(mt);
+
+            t1.start();
+
+            for (int i = 0; i <1000 ; i++) {
+                System.out.println("main thread  working : " + (char)i);
+            }
+        }
+    }
+    class MyThread extends Thread{
+        //by Extending the Thread class
+
+
+        @Override
+        public void run() {
+
+            for (int i = 1; i < 1000; i++) {
+                System.out.println(Thread.currentThread().getName() + " "+ i);
+            }
+        }
+    }
+
