4c36dc661901db33641563008abecd6a0bea4a3d
manobala <manobala2917@gmail.com>
2026-07-14 14:44:49 +0530

    pangram_2 program was created

---
+++ src/Day_13_Class_Task/pangram_2.java
@@ -0,0 +1,27 @@
+package Day_13_Class_Task;
+
+public class pangram_2 {
+        public static void main(String[] args) {
+
+            System.out.println(isPangram("The brown fox quickly jumps over The lazy dog"
+            ));
+        }
+        public static boolean isPangram(String s){
+
+
+            s= s.toLowerCase();
+
+
+            String [] al ={"a","b","c","d","e","f","g","h",
+                    "i","j","k","l","m","n","o","p","q","r","s","t","u",
+                    "v","w","x","y","z"};
+
+            for(int i=0;i<=25;i++) {
+
+                if ( s.contains(al[i] )==false){
+
+                    return false;
+                }
+            }
+            return true;
+        } }
