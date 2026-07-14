6d55110814e48c728e6c238bede1c4a68a3a5dcd
manobala <manobala2917@gmail.com>
2026-07-14 14:38:02 +0530

    the Array Deque program was created

---
+++ src/Day_18/ArrayDequeTest.java
@@ -0,0 +1,23 @@
+package Day_18;
+import java.util.ArrayDeque;
+public class ArrayDequeTest {
+
+    public static void main(String[] args){
+
+
+        ArrayDeque<Integer>dq=new ArrayDeque<>();
+
+        dq.add(5);
+        dq.add(6);
+        dq.add(3);
+        System.out.println(dq);
+        System.out.println(dq.peek());
+        System.out.println(dq.peek());
+        System.out.println(dq);
+
+    }
+
+
+
+
+}
