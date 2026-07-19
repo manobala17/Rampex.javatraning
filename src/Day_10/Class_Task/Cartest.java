59ddadc6f5b832f40d10ab6e1149013da529b13d
manobala <manobala2917@gmail.com>
2026-07-19 20:14:35 +0530

    program was created

---
+++ src/Day_10/Class_Task/Cartest.java
@@ -0,0 +1,72 @@
+package Day_10.Class_Task;
+
+public class Cartest {
+
+        public static void main(String[] args){
+            Car c1 = new Car();// first car created
+            Car c2 = new Car();
+            Car c3 = new Car();
+
+            c1.accelerate();
+            c1.accelerate();
+            c1.brake();
+
+
+            c2.brand="audi";
+            c2.accelerate();
+            c2.accelerate();
+            c2.accelerate();
+
+            c2.brake();
+            c2.brake();
+            c2.brake();
+            c2.brake();
+
+        }
+    }
+
+
+    class Car{
+
+        String brand ="bmw";
+        String model ="m5";
+
+        int speed =0;
+
+        int maxSpeed=40;
+
+
+        // methods
+        public void accelerate(){
+            if(speed==40){
+                System.out.print("cars's maximum speed reached ");
+            }else {
+
+
+                speed = speed + 5;
+                System.out.println(brand + " car is driving at : " + speed + " km speed");
+            }
+        }
+
+        public void brake(){
+
+            if(speed ==0){
+
+                System.out.print("vehicle is not moving");
+
+            }
+            else{
+
+
+                speed =speed-5;
+
+                System.out.println( brand +" brake applied  speed : "+ speed +" kms");
+            }
+
+
+
+
+        }
+
+    }
+
